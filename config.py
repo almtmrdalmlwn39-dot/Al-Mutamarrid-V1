6573d8443bc8f4378abff7c7ad6044f01c7c96a8
almtmrdalmlwn39-dot <almtmrdalmlwn39@gmail.com>
2026-05-05 14:38:48 +0300

    Add timezone and bio settings to config.py

--- config.py
+++ config.py
@@ -1,15 +1,19 @@
 import os
+import pytz
 
 # --- إعدادات المتمرد التقني ---
 API_ID = 20585941
-API_HASH = str('4c8b6debbee47ab644c82305487f34b2')
+API_HASH = "4c8b6debbee47ab644c82305487f34b2"
 BOT_TOKEN = '8687352199:AAF2STdX2zz0T_cjufv1SZHTuPrwtlzA-Gwo'
 
 # --- إعدادات المستخدم ---
 SUDO_USERS = [6467728995]
 OWNER_NAME = "المتمرد"
 OWNER_USERNAME = "Vi_ti0"
+YEMEN_TZ = pytz.timezone('Asia/Aden')
+FIXED_NAME = "فــرانــكَـَۄ|| 𝗟َِ𝗢َِ𝗿َِ𝗶َِ𝗙َِ𝒆َِل↜͟͞💸⁩"
+MY_BIO = "شخص مغرم بنفسه ولايتنازل لـ خلق الله ابدا"
 
 # --- جلب الجلسة من Render ---
-SESSION = os.environ.get("SESSION")
-
+# تأكد أن الاسم في إعدادات ريندر هو SESSION
+SESSION = os.environ.get("SESSION") 
