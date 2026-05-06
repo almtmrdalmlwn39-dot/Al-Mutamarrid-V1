33fbc55b69b07cfe6062d8cd15ddd493a8e016c0
almtmrdalmlwn39-dot <almtmrdalmlwn39@gmail.com>
2026-05-06 21:11:13 +0300

    Refactor config to use environment variables
    Updated configuration to use environment variables for API credentials.

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
-# ملف الإعدادات الخاص بسورس المتمرد
-API_ID = 20585941
-API_HASH = '4c8b6debbee47ab644c82305487f34b2'
+import os
+
+# سحب البيانات من منصة Render تلقائياً
+API_ID = int(os.environ.get("API_ID", 20585941))
+API_HASH = os.environ.get("API_HASH", "4c8b6debbee47ab644c82305487f34b2")
+SESSION = os.environ.get("SESSION")
+
+# إعدادات إضافية
+YEMEN_TZ = "Asia/Aden"
