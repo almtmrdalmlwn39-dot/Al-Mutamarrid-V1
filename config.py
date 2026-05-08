bbb950810632bc9ea974a5d2f5e4373f90417656
almtmrdalmlwn39-dot <almtmrdalmlwn39@gmail.com>
2026-05-08 18:06:46 +0300

    Revise API settings for enhanced privacy
    Updated API configuration to enhance privacy and security.

--- config.py
+++ config.py
@@ -1,9 +1,19 @@
 import os
 
-# سحب البيانات من منصة Render تلقائياً
-API_ID = int(os.environ.get("API_ID", 20585941))
-API_HASH = os.environ.get("API_HASH", "4c8b6debbee47ab644c82305487f34b2")
-SESSION = os.environ.get("SESSION")
+# --- [ إعدادات سورس 𝗔𝗟-𝗠𝗨𝗧𝗔𝗠𝗔𝗥𝗥𝗜𝗗 𝗧𝗘𝗖𝗛 ] ---
 
-# إعدادات إضافية
+# سحب البيانات إجبارياً من منصة الاستضافة (Render/Heroku)
+# الآن لا توجد أي أرقام أو بيانات حقيقية هنا، الخصوصية 100%
+API_ID = int(os.environ.get("API_ID", 0)) 
+API_HASH = os.environ.get("API_HASH", None)
+SESSION = os.environ.get("SESSION", None)
+
+# سحب ايديك وايدي المطورين من المنصة فقط
+SUDO_STR = os.environ.get("SUDO_USERS", "")
+SUDO_USERS = list(map(int, SUDO_STR.split())) if SUDO_STR else []
+
+# إعدادات الوقت
 YEMEN_TZ = "Asia/Aden"
+
+# اسم السورس الرسمي
+BOT_NAME = "𝗔𝗟-𝗠𝗨𝗧𝗔𝗠𝗔𝗥𝗥𝗜𝗗 𝗧𝗘𝗖𝗛"
