f842771989f421e0bcb2ca398383686e68b08cd9
almtmrdalmlwn39-dot <almtmrdalmlwn39@gmail.com>
2026-05-20 18:18:24 +0300

    Convert bot to use asynchronous main function
    Refactor bot initialization to use async main function.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
+import asyncio
 from pyrogram import Client
 
-# بياناتك
+# بيانات AL-MUTAMARRID
 API_ID = 22610186
 API_HASH = "184e7fd176413cd0d2425494f1796229"
 BOT_TOKEN = "8794844755:AAHA6yvFrM2rEm6A2II1LlenrFOl8ZjfGVE"
 
-# إعداد البوت باسم فخم وتنسيق احترافي
 app = Client(
     "AL-MUTAMARRID-V2",
     api_id=API_ID,
@@ -14,9 +14,13 @@ app = Client(
     plugins=dict(root="plugins")
 )
 
-if __name__ == "__main__":
+async def main():
+    await app.start()
     print("-----------------------------------------")
     print("🛡️  AL-MUTAMARRID SOURCE IS NOW LIVE!  🛡️")
-    print("🚀 THE REBEL PROJECT STARTED SUCCESSFULLY")
     print("-----------------------------------------")
-    app.run()
+    await asyncio.Event().wait()
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
